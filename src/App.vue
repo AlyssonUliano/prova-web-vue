17e8cc8d441a7533804c2efb590d22ea89983201
AlyssonUliano <55817018+AlyssonUliano@users.noreply.github.com>
2020-06-23 23:29:30 -0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="app">
+    <BuscarCep />
+  </div>
+</template>
+
+<script>
+import BuscarCep from "./components/BuscarCep.vue";
+
+export default {
+  name: "App",
+  components: {
+    BuscarCep
+  }
+};
+</script>
+
+<style>
+body {
+  background: rgb(217, 243, 225);
+}
+
+#app {
+  font-family:  Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: arial;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: left;
+  color: #57a2ee;
+  margin-top: 60px;
+}
+
+h1,
+h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #000000;
+}
+</style>
